1bbca230f476408cd8ac9f5a5697aa41d5f50fd2
jimlinJC <jinchglin@gmail.com>
2020-12-15 18:07:41 +0800

    add popup.js

---
+++ popup.js
@@ -0,0 +1,10 @@
+var slider = document.getElementById("range");
+var output = document.getElementById("output");
+output.innerHTML = slider.value; // Display the default slider value
+var sendMessage = (messageObj) => chrome.tabs.sendMessage(tabId, messageObj);
+// Update the current slider value (each time you drag the slider handle)
+slider.oninput = function() {
+  output.innerHTML = this.value;
+  sendMessage({value : this.value})
+}
+chrome.tabs.getSelected(null, getSelectedTab);
