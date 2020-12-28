0413d1f31cd9835e3bd5c7b6777de4a0a4649ad7
jimlinJC <jinchglin@gmail.com>
2020-12-29 00:22:07 +0800

    feat: init slider value

--- exe.js
+++ exe.js
@@ -1,6 +1,16 @@
-  
-  const onMessage = (message) => {
+
+const onMessage = (message, sender, sendResponse) => {
+  if (message.value == -1) {
+    sendResponse({
+      content: document.querySelector("video").playbackRate.toString(),
+    });
+  } else {
     document.querySelector("video").playbackRate = message.value;
+    sendResponse({
+      content: 'success!',
+    });
   }
-  
-  chrome.runtime.onMessage.addListener(onMessage);
+
+}
+
+chrome.runtime.onMessage.addListener(onMessage);
--- popup.js
+++ popup.js
@@ -1,10 +1,22 @@
+var initialize = true;
+
 var getSelectedTab = (tab) => {
     var tabId = tab.id;
     var slider = document.getElementById("range");
     var output = document.getElementById("output");
     output.innerHTML = slider.value; // Display the default slider value
-    var sendMessage = (messageObj) => chrome.tabs.sendMessage(tabId, messageObj);
-    // Update the current slider value (each time you drag the slider handle)
+    var sendMessage = (messageObj) => chrome.tabs.sendMessage(tabId, messageObj, function (response) {
+        if (response.content != "success!") {
+            output.innerHTML = response.content;
+            slider.value = response.content;
+        }
+    });
+
+    if (initialize) {
+        sendMessage({ value: -1 });
+        initialize = false;
+    }
+    
     slider.oninput = function () {
         output.innerHTML = this.value;
         sendMessage({ value: this.value })
